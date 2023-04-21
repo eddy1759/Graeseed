a9deb6fadc384f7fd8abd176bfdf34b14ea023bb
Edet Asuquo <eddy1759@gmail.com>
2023-04-21 11:05:30 +0100

    WIP

--- BG-Workers/bookRouter.js
+++ BG-Workers/bookRouter.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const { Queue } = require('bull')
+const { Queue, Job } = require('bull')
 const searchJob = require('./workers/searchJob')
 const { getAllBooks, getBookById, addBook, updateBook, deleteBook } = require('./bookController')
 
 const bookRouter = express.Router()
-
+const searchQueue = new Queue('search');
 
 bookRouter.get('/', getAllBooks)
 bookRouter.get('/:id', getBookById)
@@ -15,8 +15,18 @@ bookRouter.delete('/:id', deleteBook)
 
 bookRouter.get('/search', async (req, res) => {
     const query = req.query.q
-    const job = await searchQu
+    const job = await searchQueue.add({query})
+    res.json({ jobId: job.id })
 })
 
+bookRouter.get('/:jobId', async (req, res) => {
+    const jobId = req.params.jobId;
+    const job = await Job.fromId(searchQueue, jobId);
+    if (job) {
+      res.json({ status: job.status, result: job.returnvalue });
+    } else {
+      res.status(404).json({ message: `Job ${jobId} not found` });
+    }
+});
 
 module.exports = bookRouter
