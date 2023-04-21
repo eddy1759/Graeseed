2afecc60e1e2e2da70d2124994f9647d09e736c7
Edet Asuquo <eddy1759@gmail.com>
2023-04-21 11:02:10 +0100

    WIP

--- BG-Workers/bookRouter.js
+++ BG-Workers/bookRouter.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const { Queue } = require('bull')
+const searchJob = require('./workers/searchJob')
 const { getAllBooks, getBookById, addBook, updateBook, deleteBook } = require('./bookController')
 
 const bookRouter = express.Router()
@@ -9,5 +11,12 @@ bookRouter.get('/:id', getBookById)
 bookRouter.post('/', addBook)
 bookRouter.put('/:id', updateBook)
 bookRouter.delete('/:id', deleteBook)
+
+
+bookRouter.get('/search', async (req, res) => {
+    const query = req.query.q
+    const job = await searchQu
+})
+
 
 module.exports = bookRouter
